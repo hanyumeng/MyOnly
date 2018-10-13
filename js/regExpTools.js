e238660ee247e00bb3232c57aca5874194ee5067
LcHym <1183725330@qq.com>
2018-10-13 08:57:59 +0800

    第四个版本加入index2.0.html和分页1登录页面

---
+++ js/regExpTools.js
@@ -0,0 +1,29 @@
+
+
+//正则封装
+//参数：
+//要验证的字符串:str
+//要验证的规则：type(email：表示邮箱；phone：表示手机号码)
+
+//check("hello","email");
+
+function check(str,type){
+	switch(type){
+		case "userName":var reg = /^[a-zA-Z_]\w{5,14}$/; break;
+		case "email":var reg = /^[a-zA-Z0-9]+@[a-zA-Z0-9]+(\.com|\.cn|\.net)$/; break;
+		case "phone":var reg = /^1[3-9]\d{9}$/;break;
+		case "post":var reg =  /^[1-9]\d{5}$/;break;
+		
+		default:;
+	}
+	return reg.test(str);
+}
+function $(str){
+	if(str.charAt(0)=="#"){
+		return document.getElementById(str.substring(1));
+	}else if(str.charAt(0)=="."){
+		return document.getElementsByClassName(str.substring(1));
+	}else{
+		return document.getElementsByTagName(str);
+	}
+}
